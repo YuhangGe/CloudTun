1edd5f7ac5f1bc29d9065d98a31f1feeef6f465e
yuhang <abeyuhang@gmail.com>
2025-09-25 18:04:29 +0800

    update

--- src-vpn/src/vpn.rs
+++ src-vpn/src/vpn.rs
@@ -1,8 +1,9 @@
+use std::net::SocketAddr;
 use std::sync::Arc;
 
 use cloudtun_common::proxy::{generate_proxy_secret, proxy_to_cloudtun_server};
 use ipstack::{IpStackStream, IpStackUdpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::{
   io::{AsyncRead, AsyncWrite},
   sync::Mutex,
@@ -13,7 +14,7 @@ use std::str::FromStr;
 use tproxy_config::IpCidr;
 
 // use tproxy_config::is_private_ip;
-// use udp_stream::UdpStream;
+use udp_stream::UdpStream;
 
 use crate::virtual_dns::VirtualDns;
 
@@ -49,7 +50,7 @@ pub async fn start_run_vpn<D, F: Fn(&str, &str) + Send + Sync + 'static>(
   server_addr: (String, u16, String),
   shutdown_token: CancellationToken,
   log_fn: F,
-) -> anyhow::Result<usize>
+) -> anyhow::Result<()>
 where
   D: AsyncRead + AsyncWrite + Unpin + Send + 'static,
 {
@@ -65,9 +66,6 @@ where
 
   let mut ip_stack = ipstack::IpStack::new(ipstack_config, device);
 
-  let task_count = std::sync::Arc::new(std::sync::atomic::AtomicUsize::new(0));
-  use std::sync::atomic::Ordering::Relaxed;
-
   let secret = Arc::new(generate_proxy_secret());
   let server_addr = Arc::new(server_addr);
 
@@ -75,7 +73,6 @@ where
     let secret = secret.clone();
     let log_fn = log_fn.clone();
     let server_addr = server_addr.clone();
-    let task_count = task_count.clone();
     let virtual_dns = virtual_dns.clone();
     let ip_stack_stream = tokio::select! {
         _ = shutdown_token.cancelled() => {
@@ -86,27 +83,9 @@ where
             ip_stack_stream?
         }
     };
-    // println!(
-    //   "got ip stack {} {}",
-    //   match ip_stack_stream {
-    //     IpStackStream::Tcp(_) => "<tcp>",
-    //     IpStackStream::Udp(_) => "<udp>",
-    //     IpStackStream::UnknownNetwork(_) => "<unknown net>",
-    //     IpStackStream::UnknownTransport(_) => "<unknown tran>",
-    //   },
-    //   ip_stack_stream.peer_addr()
-    // );
+
     match ip_stack_stream {
       IpStackStream::Tcp(tcp) => {
-        if task_count.load(Relaxed) >= 200 {
-          log::error!("Too many sessions, ignore...");
-          continue;
-        }
-        // println!(
-        //   "Session count {}",
-        //   task_count.fetch_add(1, Relaxed).saturating_add(1)
-        // );
-        // let info = SessionInfo::new(tcp.local_addr(), tcp.peer_addr(), IpProtocol::Tcp);
         let target_ip = tcp.peer_addr().ip();
         let target_port = tcp.peer_addr().port();
         let domain_name = {
@@ -128,63 +107,71 @@ where
           {
             eprintln!("failed proxy: \"{err}\"");
           }
-          // println!(
-          //   "Session count {}",
-          //   task_count.fetch_sub(1, Relaxed).saturating_sub(1)
-          // );
         });
       }
-      IpStackStream::Udp(udp) => {
-        if task_count.load(Relaxed) >= 200 {
-          log::error!("Too many udp sessions");
-          continue;
-        }
-        println!(
-          "Session count {}",
-          task_count.fetch_add(1, Relaxed).saturating_add(1)
-        );
-        // let info = SessionInfo::new(udp.local_addr(), udp.peer_addr(), IpProtocol::Udp);
+      IpStackStream::Udp(mut udp) => {
         if udp.peer_addr().port() == DNS_PORT {
-          //   if is_private_ip(info.dst.ip()) {
-          //     info.dst.set_ip(dns_addr); // !!! Here we change the destination address to remote DNS server!!!
-          //   }
-
           tokio::spawn(async move {
             if let Err(err) = handle_virtual_dns_session(udp, virtual_dns).await {
               eprintln!("failed handler virtual dns: \"{err}\"");
             }
-            // println!(
-            //   "Session count {}",
-            //   task_count.fetch_sub(1, Relaxed).saturating_sub(1)
-            // );
           });
-          continue;
+        } else {
+          tokio::spawn(async move {
+            let target_ip = udp.peer_addr().ip();
+            let target_port = udp.peer_addr().port();
+            let target = format!("{}:{}", target_ip, target_port);
+            let mut target_stream =
+              match UdpStream::connect(SocketAddr::from_str(&target).unwrap()).await {
+                Err(e) => {
+                  eprintln!("failed create udp stream");
+                  return;
+                }
+                Ok(x) => x,
+              };
+
+            let mut buf1 = [0_u8; 4096];
+            let mut buf2 = [0_u8; 4096];
+            loop {
+              tokio::select! {
+                len = udp.read(&mut buf1) => {
+                  let len = match len {
+                    Ok(n) => n,
+                    Err(e) => {
+                      eprintln!("{e}");
+                      break;
+                    }
+                  };
+                  if len == 0 {
+                      break;
+                  }
+                  let buf1 = &buf1[..len];
+                  if let Err(e) = target_stream.write_all(buf1).await {
+                    eprintln!("{e}");
+                    break;
+                  }
+                },
+                len = target_stream.read(&mut buf2) => {
+                  let len = match len {
+                    Ok(n) => n,
+                    Err(e) => {
+                      eprintln!("{e}");
+                      break;
+                    }
+                  };
+                  if len == 0 {
+                      break;
+                  }
+                  let buf2 = &buf2[..len];
+                  if let Err(e) = udp.write_all(buf2).await {
+                    eprintln!("{e}");
+                    break;
+                  }
+                }
+              }
+            }
+          });
         }
-
-        // let domain_name = {
-        //   let mut virtual_dns = virtual_dns.lock().await;
-        //   virtual_dns.touch_ip(&udp.peer_addr().ip());
-        //   virtual_dns.resolve_ip(&udp.peer_addr().ip()).cloned()
-        // };
-
-        // match mgr.new_proxy_handler(info, domain_name, true).await {
-        //   Ok(proxy_handler) => {
-        //     // let socket_queue = socket_queue.clone();
-        //     tokio::spawn(async move {
-        //       // let ty = args.proxy.proxy_type;
-        //       if let Err(err) = handle_udp_associate_session(udp, proxy_handler).await {
-        //         eprintln!("Ending {info} with \"{err}\"");
-        //       }
-        //       println!(
-        //         "Session count {}",
-        //         task_count.fetch_sub(1, Relaxed).saturating_sub(1)
-        //       );
-        //     });
-        //   }
-        //   Err(e) => {
-        //     eprintln!("Failed to create UDP connection: {e}");
-        //   }
-        // }
       }
       IpStackStream::UnknownTransport(u) => {
         let len = u.payload().len();
@@ -193,13 +180,11 @@ where
           u.ip_protocol(),
           len
         );
-        continue;
       }
       IpStackStream::UnknownNetwork(pkt) => {
         println!("#0 unknown transport - {} bytes", pkt.len());
-        continue;
       }
     }
   }
-  Ok(task_count.load(Relaxed))
+  Ok(())
 }
