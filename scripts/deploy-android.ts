685b6d397abbf6b8668d8d0f06214c71d83c7fda
yuhang <abeyuhang@gmail.com>
2025-09-28 12:46:52 +0800

    fix

--- scripts/deploy-android.ts
+++ scripts/deploy-android.ts
@@ -6,8 +6,13 @@ import { $ } from 'bun';
 const CWD = process.cwd();
 
 async function run() {
-  await $`rm dist/cloudtun.apks dist/cloudtun-release.apk`;
-  const cmd = `java -jar ${path.join(CWD, 'bundletool-all-1.18.1.jar')} build-apks --bundle=${path.join(CWD, 'src-tauri/gen/android/app/build/outputs/bundle/universalRelease/app-universal-release.aab')} --output=${path.join(CWD, 'dist/cloudtun.apks')} --mode=universal --ks=${path.join(CWD, 'scripts/key.jks')} --ks-pass=pass:a123456 --ks-key-alias=key --key-pass=pass:a123456`;
+  try {
+    await $`rm dist/cloudtun.apks dist/cloudtun-release.apk`;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    // ignore
+  }
+  const cmd = `java -jar ${path.join(CWD, 'scripts/bundletool-all-1.18.1.jar')} build-apks --bundle=${path.join(CWD, 'src-tauri/gen/android/app/build/outputs/bundle/universalRelease/app-universal-release.aab')} --output=${path.join(CWD, 'dist/cloudtun.apks')} --mode=universal --ks=${path.join(CWD, 'scripts/key.jks')} --ks-pass=pass:a123456 --ks-key-alias=key --key-pass=pass:a123456`;
   console.info('\n\nApks generated.\n\nExtracting apk...\n');
   await execSync(cmd);
   await unzipFile(path.join(CWD, 'dist/cloudtun.apks'), path.join(CWD, 'dist'));
